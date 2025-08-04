7b9de7bb617356d38d0c2c3128bd963d6735034b
isabellopez2005 <isa052207@gmail.com>
2025-08-04 13:32:29 -0500

    3_condicionales

---
+++ app.js
@@ -0,0 +1,8 @@
+let estatura = parselnt(prompt("ingrese su estatura en cm"));
+let velocidad = parselnt(prompt("ingrese su velocidad en km/h"));
+let edad = parselnt (prompt("ingrese su edad"));
+
+if (estatura >=170 && velocidad >= 27) {
+    alert("cumples con los requisitos para ingresar al equipo");
+    
+}
